3742367e4a24524b06af1d6f01414d82bfcafa96
DucksIncoming <ducksincoming@gmail.com>
2022-08-17 02:56:27 -0400

    Merge to main file

--- MechArm/MechArm.ino
+++ MechArm/MechArm.ino
@@ -1,69 +1,127 @@
-/*
-  Movement-Tracking Mech Arm by DucksIncoming
-  v1.0
-  https://github.com/DucksIncoming/Mechanical-Tracking-Arm
-  https://www.youtube.com/channel/UCwgpVWzXKdMB-kHblUzMsLg
- */
-
 #include <Wire.h>
-#include <MPU6050.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 
-/* ============= ! PLEASE NOTE ! ====================
-  This program uses a modified version of the
-  Arduino-MPU6050 library, originally obtained
-  from https://github.com/jarzebski/Arduino-MPU6050.
-  This was just the easiest way to do this, just
-  make sure you either download the modified library
-  from my GitHub repo or change the MPU6050_ADDRESS
-  variable to 0x70 instead of 0x68, or whatever the
-  I2C address of your TCA9548A or equivalent is.
-===================================================== */
+const int TCA_ADDRESS = 0x70;
 
-MPU6050 mpu;
+Adafruit_MPU6050 mpu;
+float yaw, pitch, roll;
+float curTime, prevTime, deltaTime;
 
-unsigned long timer = 0;
-float timeStep = 0.01;
-
-// Pitch, Roll and Yaw values
-float pitch = 0;
-float roll = 0;
-float yaw = 0;
+float GyroXError[3];
+float GyroYError[3];
+float GyroZError[3];
 
 void TCA9548A(uint8_t bus){
-  Wire.beginTransmission(0x70);
+  Wire.beginTransmission(TCA_ADDRESS);
   Wire.write(1 << bus);
   Wire.endTransmission();
-  Serial.print(bus);
+  //Serial.print(bus);
 }
 
-void setup() 
-{
-  TCA9548A(1);
-  Serial.begin(115200);
-  while(!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G))
-  {
-    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
-    delay(500);
+void calibrateGyro(int gyroIndex, int depth) {
+  Serial.println("Calibrating gyros...");
+  Serial.println("WARNING: Moving the MPU6050 device while calibrating will result in incorrect behavior.");
+
+  float Xreadings, Yreadings, Zreadings;
+
+  // X Calibration
+  for (int i = 0; i < depth; i++){
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    Xreadings += g.gyro.x;
+    delay(3);
   }
-  mpu.calibrateGyro();
-  mpu.setThreshold(3);
+  Serial.print(".");
+
+  // Y Calibration
+  for (int i = 0; i < depth; i++){
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    Yreadings += g.gyro.y;
+    delay(3);
+  }
+  Serial.print(".");
+
+  // Z Calibration
+  for (int i = 0; i < depth; i++){
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    Zreadings += g.gyro.z;
+    delay(3);
+  }
+  Serial.println(".");
+
+  switch (gyroIndex) {
+    case 0: {
+      GyroXError[0] = Xreadings / depth;
+      GyroYError[0] = Yreadings / depth;
+      GyroZError[0] = Zreadings / depth;
+      break;
+    }
+    case 1: {
+      GyroXError[1] = Xreadings / depth;
+      GyroYError[1] = Yreadings / depth;
+      GyroZError[1] = Zreadings / depth;
+      break;
+    }
+    case 2: {
+      GyroXError[2] = Xreadings / depth;
+      GyroYError[2] = Yreadings / depth;
+      GyroZError[2] = Zreadings / depth;
+      break;
+    }
+  }
 }
 
-void loop()
-{
-  TCA9548A(1);
-  timer = millis();
-  Vector norm = mpu.readNormalizeGyro();
-  pitch = pitch + norm.YAxis * timeStep;
-  roll = roll + norm.XAxis * timeStep;
-  yaw = yaw + norm.ZAxis * timeStep;
+void setup() {
+  Serial.begin(115200);
+  
+  for (int i = 0; i < 3; i++){
+    TCA9548A(i);
+    
+    while (!mpu.begin()){
+      Serial.println("No MPU6050 connected, or wiring is incorrect.");
+      delay(10);
+    }
+    Serial.println("MPU6050 Found (" + String(i + 1) + "/3): Starting...");
+  
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
-  Serial.print(" Pitch = ");
-  Serial.print(pitch);
-  Serial.print(" Roll = ");
-  Serial.print(roll);  
-  Serial.print(" Yaw = ");
-  Serial.println(yaw);
+    calibrateGyro(i, 200); // Second argument is depth, keep high for accuracy.
+    delay(100);
+  }
 
-  delay((timeStep*1000) - (millis() - timer));
+  roll = 0;
+  pitch = 0;
+  yaw = 0;
+  curTime = millis();
+}
+
+void loop() {
+  for (int i = 0; i < 3; i++){
+    TCA9548A(i);
+    prevTime = curTime;
+    curTime = millis();
+    deltaTime = (curTime - prevTime) / 1000;
+    
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+  
+    roll += ((g.gyro.x - GyroXError[i]) * deltaTime) * (180/PI);
+    pitch += ((g.gyro.y - GyroYError[i]) * deltaTime) * (180/PI);
+    yaw += ((g.gyro.z - GyroZError[i]) * deltaTime) * (180/PI);
+
+    Serial.print(">" + String(i) + "<");
+    Serial.print(",");
+    Serial.print(yaw);
+    Serial.print(",");
+    Serial.print(pitch);
+    Serial.print(",");
+    Serial.println(roll);
+  
+    delay(3);
+  }
 }
